3290e395a1012830cd058f8885e9c09defa78f50
Phil Welsch <pwelsch86@gmail.com>
2022-08-16 23:20:26 +0100

    in progress parsing work

--- src/tear_app.rs
+++ src/tear_app.rs
@@ -1,4 +1,4 @@
-use std::{process::{Command, Stdio}, io::{Read, BufReader, BufRead}};
+use std::{process::{Command, Stdio}, io::{Read, BufReader, BufRead, Write}, fs::File};
 
 #[derive(Default)]
 pub struct App {
@@ -14,6 +14,70 @@ pub struct App {
 impl App {
     pub fn new() -> App {
         Default::default()
+    }
+
+    fn parse_and_render_text(&mut self, ui: &mut egui::Ui) {
+        let mut rg_out = Command::new("rg")
+            .stdout(Stdio::piped())
+            .arg("search_res") // emoji test 🍳🍳🍳🦝🦝🦝 !!
+            .arg("--pretty")
+            .spawn().unwrap()
+            .stdout.unwrap();
+
+        let mut out_str = String::new();
+        rg_out.read_to_string(&mut out_str);
+
+        #[derive(Default)]
+        struct EscapeSequence {
+
+        }
+
+        let mut token = String::new();
+        let mut escape_stack = Vec::new();
+
+        enum ParseState {
+            None,
+            Escape,
+        }
+
+        let mut state = ParseState::None;
+
+        for c in out_str.chars() {
+            if c.is_control() {
+                match state {
+                    ParseState::None => {
+                        escape_stack.push(EscapeSequence::default());
+                        state = ParseState::Escape
+                    },
+                    ParseState::Escape => panic!("Unexpected escape character in escape sequence."),
+                }
+            }
+            else {
+                match state {
+                    ParseState::None => token.push(c),
+                    ParseState::Escape => {
+                        match c {
+                            '[' => (), // open the escape sequence
+                            // handle digit for color code
+                            // m to terminate the sequence
+                            _ => {
+                                println!("Unhandled character {c} in escape sequence, reverting to treating this is a token");
+                                state = ParseState::None;
+                                token.push(c);
+                            },
+                        }
+                    },
+                }
+            } 
+        }
+
+        // out_str.as_bytes().into_iter().for_each(|b| {
+        //     let c = *b as char;
+        //     print!("{} ", c);
+        // });
+
+        panic!();
+        // ui.selectable_label(false, out_str);
     }
 
     pub fn update(&mut self, ctx: &egui::Context, frame: &epi::Frame) {
@@ -45,65 +109,53 @@ impl App {
                 res
             });
 
-            if search_res.inner.changed() {
-                let search_dir = if self.dir_text.is_empty() {
-                    ".".to_string()
-                } else { 
-                    self.dir_text.clone() 
-                };
+            self.parse_and_render_text(ui);
 
-                let mut rg_out = Command::new("rg")
-                .stdout(Stdio::piped())
-                .arg(self.search_text.clone())
-                // .arg("--pretty")
-                .arg("--vimgrep")
-                .current_dir(search_dir)
-                .spawn().unwrap()
-                .stdout.unwrap();
+            // if search_res.inner.changed() {
+            //     let search_dir = if self.dir_text.is_empty() {
+            //         ".".to_string()
+            //     } else { 
+            //         self.dir_text.clone() 
+            //     };
+
+            //     let mut rg_out = Command::new("rg")
+            //     .stdout(Stdio::piped())
+            //     .arg(self.search_text.clone())
+            //     .arg("--pretty")
+                // .arg("--vimgrep")
+                // .current_dir(search_dir)
+                // .spawn().unwrap()
+                // .stdout.unwrap();
     
-                let reader = BufReader::new(rg_out);
-                self.output_lines = BufRead::lines(reader)
-                    .filter_map(|line| line.ok())
-                    .collect();
+                // let reader = BufReader::new(rg_out);
+                // self.output_lines = BufRead::lines(reader)
+                //     .filter_map(|line| line.ok())
+                //     .collect();
 
                 // let mut out_str = String::new();
                 // rg_out.read_to_string(&mut out_str);
                 // self.output_lines.push(out_str);
-            }
+                // println!("{}", out_str);
 
-            egui::ScrollArea::vertical().show(ui, |ui| {
-                for line in &self.output_lines {
-                    ui.selectable_label(false, line);
-                    ui.separator();
-                }
-            });
+                // self.output_lines = out_str.split("\n\n")
+                //     .into_iter()
+                //     .map(|slc| slc.to_string())
+                //     .collect::<Vec<_>>();
+            // }
+
+            // egui::ScrollArea::vertical().show(ui, |ui| {
+                // for line in &self.output_lines {
+                //     ui.selectable_label(false, line);
+                //     ui.separator();
+                // }
+            // });
         });
 
-        egui::SidePanel::right("test_right_panel").show(ctx, |ui| {
-            ui.label("App Log");
-            ui.separator();
+        // egui::SidePanel::right("test_right_panel").show(ctx, |ui| {
+        //     ui.label("App Log");
+        //     ui.separator();
             
-            ui.label("log goes here");
-        });
-
-        // match rg_process.wait() {
-        //     Ok(_) => (),
-        //     Err(_) => println!("error waiting for rg"),
-        // }
-
-        // rg_process.
-
-        // rg_process.stdout.unwrap().read_to_string(buf)
-
-        // .kill()
-        // .output()
-        // .expect("failed to run ripgrep"); // handle more gracefully, check it works when first launching
-
-
-        // egui::Window::new("tear_app")
-        //     .open(&mut self.is_open)
-        //     .show(ctx, |ui| {
-                
-        //     });
+        //     ui.label("log goes here");
+        // });
     }
 }
