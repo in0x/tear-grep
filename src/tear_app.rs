e8bb62d96f171afaf30971dc068c9f8f7de2d98d
Phil Welsch <pwelsch86@gmail.com>
2022-08-17 13:32:15 +0100

    continue parser work

--- src/tear_app.rs
+++ src/tear_app.rs
@@ -1,4 +1,4 @@
-use std::{process::{Command, Stdio}, io::{Read, BufReader, BufRead, Write}, fs::File};
+use std::{process::{Command, Stdio}, io::{Read, BufReader, BufRead, Write}};
 
 #[derive(Default)]
 pub struct App {
@@ -28,47 +28,91 @@ impl App {
         rg_out.read_to_string(&mut out_str);
 
         #[derive(Default)]
-        struct EscapeSequence {
-
+        struct TextSegment {
+            text: String,
+            color_code: Option<i32>,
+            bold: bool
         }
-
-        let mut token = String::new();
-        let mut escape_stack = Vec::new();
-
+        
         enum ParseState {
             None,
             Escape,
         }
 
+        let mut cur_segment = TextSegment::default();
+        let mut completed_segments : Vec<TextSegment> = Vec::new();
+
         let mut state = ParseState::None;
+        let mut escape_code = 0;
 
         for c in out_str.chars() {
-            if c.is_control() {
-                match state {
-                    ParseState::None => {
-                        escape_stack.push(EscapeSequence::default());
+            match state {
+                ParseState::None => {
+                    if c.is_control() {
                         state = ParseState::Escape
-                    },
-                    ParseState::Escape => panic!("Unexpected escape character in escape sequence."),
+                    }
+                    else {
+                        cur_segment.text.push(c);
+                    }
                 }
+                // if we find two newlines, break into a new filematch
+                ParseState::Escape => {
+                    match c {
+                        '[' => (), // open the escape sequence
+                        'm' =>  {
+                            // handle the escape code and close this escape sequence
+                            match escape_code {
+                                0 => {
+                                    if !cur_segment.text.is_empty() {
+                                        completed_segments.push(cur_segment);
+                                    }
+                                    cur_segment = TextSegment::default();
+                                },
+                                1 => cur_segment.bold = true,
+                                _ => cur_segment.color_code = Some(escape_code),
+                            }
+
+                            escape_code = 0;
+                            state = ParseState::None;
+                        },
+                        '0'..='9' => {
+                            let num = match c.to_digit(10) {
+                                Some(d) => d as i32,
+                                None => {
+                                    println!("Failed to convert digit to its numeric value.");
+                                    0
+                                }
+                            };
+                            escape_code *= 10;
+                            escape_code += num;
+                        },
+                        // handle digit for color code
+                        // m to terminate the sequence
+                        _ => {
+                            println!("Unhandled character {c} in escape sequence, reverting to treating this as a token");
+                            state = ParseState::None;
+                            cur_segment.text.push(c);
+                        },
+                    }
+                },
             }
-            else {
-                match state {
-                    ParseState::None => token.push(c),
-                    ParseState::Escape => {
-                        match c {
-                            '[' => (), // open the escape sequence
-                            // handle digit for color code
-                            // m to terminate the sequence
-                            _ => {
-                                println!("Unhandled character {c} in escape sequence, reverting to treating this is a token");
-                                state = ParseState::None;
-                                token.push(c);
-                            },
-                        }
-                    },
-                }
-            } 
+        }
+
+        // try pushing the last segment, since it may not have been terminated by an escape sequence
+        if !cur_segment.text.is_empty() {
+            completed_segments.push(cur_segment);
+        }
+
+        for seg in &completed_segments {
+            print!("Style: ");
+            if seg.bold {
+                print!("bold ");
+            }
+            
+            seg.color_code.and_then(|val| { print!(" color: {val}"); Some(val) } );
+
+            print!("\n");
+            println!("Text: {}", seg.text);
         }
 
         // out_str.as_bytes().into_iter().for_each(|b| {
